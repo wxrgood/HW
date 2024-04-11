92cb29cf6982a3814acc20dd7465d39a5a448cbc
wxrgood <2859255125tank@gmail.com>
2024-04-11 18:52:15 -0400

    Update BondPrice_E_File.py

--- BondPrice_E_File.py
+++ BondPrice_E_File.py
@@ -1,4 +1,12 @@
 
 
 def getBondPrice_E(face, couponRate, m, yc):
-    return(2098949)
+    bondPrice = 0t
+    coupon_payment = face * couponRate
+    for t, y in enumerate(yc, start=1):
+        bondPrice += coupon_payment / ((1 + y)**t)
+        if t == m:
+            bondPrice += face / ((1 + y)**t)
+            
+    return bondPrice
+

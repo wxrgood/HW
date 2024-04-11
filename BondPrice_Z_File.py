61062ec1a72d441f2b3629002b572326e1515b84
wxrgood <2859255125tank@gmail.com>
2024-04-11 19:01:52 -0400

    Update BondPrice_Z_File.py

--- BondPrice_Z_File.py
+++ BondPrice_Z_File.py
@@ -1,4 +1,10 @@
 
 
 def getBondPrice_Z(face, couponRate, times, yc):
-    return(1996533)
+    bondPrice = 0
+    coupon_payment = face * couponRate
+    for time, yield_rate in zip(times, yc):
+        bondPrice += coupon_payment / ((1 + yield_rate)**time)
+    bondPrice += face / ((1 + yc[-1])**times[-1])
+
+    return bondPrice
